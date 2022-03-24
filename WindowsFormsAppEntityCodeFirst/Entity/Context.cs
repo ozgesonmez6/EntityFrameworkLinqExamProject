7d6c800aec8ab978bb954613aa77356358bda2e7
ozgesonmez6 <ozgesonmez_6@hotmail.com>
2022-03-24 22:40:59 +0300

    Code First Usage

---
+++ WindowsFormsAppEntityCodeFirst/Entity/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppEntityCodeFirst.Entity
+{
+    public class Context:DbContext
+    {
+        public DbSet<Products> Products { get; set; }
+
+    }
+}
---
+++ WindowsFormsAppEntityCodeFirst/Entity/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppEntityCodeFirst.Entity
+{
+    public class Products
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductMarka { get; set; }
+        public string ProductCategory { get; set; }
+        public int ProductStok { get; set; }
+    }
+}
