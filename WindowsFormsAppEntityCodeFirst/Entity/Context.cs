00e2077340abf2d5d0052669001e46146898b2a3
ozgesonmez6 <ozgesonmez_6@hotmail.com>
2022-03-25 12:15:36 +0300

    Adding Tables via Code First Migrations

--- WindowsFormsAppEntityCodeFirst/Entity/Context.cs
+++ WindowsFormsAppEntityCodeFirst/Entity/Context.cs
@@ -12,6 +12,7 @@ namespace WindowsFormsAppEntityCodeFirst.Entity
         public DbSet<Products> Products { get; set; }
 
         public DbSet<Categories> Categories { get; set; }
+        public DbSet<Customers> Customers { get; set; }
 
     }
 }
---
+++ WindowsFormsAppEntityCodeFirst/Entity/Customers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppEntityCodeFirst.Entity
+{
+    public class Customers
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerLastname { get; set; }
+    }
+}
